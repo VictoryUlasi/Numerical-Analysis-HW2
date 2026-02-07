c148d9fc929b83bd5437389d69912b48bdaf45b0
Vic Laptop <victoryulasi@protonmail.ch>
2026-02-06 18:15:22 -0600

    moved p8 to separate files

--- p8_factorial.c
+++ p8_factorial.c
@@ -1,35 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-const double pi = 3.14159265358979323846;
-
 double p8_factorial(int n){
 
     if (n == 0)
         return 1;
     
     return n * p8_factorial(n-1);
-}
-
-double p8_sin_taylor(double x , int n){
-
-    if (n < 0)
-        return 0.0;
-
-    double prev = pow(-1,n) * (pow(x,((2*n) + 1)) / p8_factorial((2*n) + 1));
-
-    return (prev + p8_sin_taylor(x , n-1));
-}
-
-int main(){
-
-    double x;
-    int N;
-
-    if(scanf("%lf %d", &x, &N) == 0){
-        return 1;
-    }
-    printf("%lf\n",p8_sin_taylor(x,N));
-
-    return 0;
 }
---
+++ p8_sin_taylor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include "p8_factorial.c"
+
+double p8_sin_taylor(double x , int n){
+
+    if (n < 0)
+        return 0.0;
+
+    double prev = pow(-1,n) * (pow(x,((2*n) + 1)) / p8_factorial((2*n) + 1));
+
+    return (prev + p8_sin_taylor(x , n-1));
+}
+
+int main(){
+
+    double x;
+    int N;
+
+    if(scanf("%lf %d", &x, &N) == 0){
+        return 1;
+    }
+    printf("%lf\n",p8_sin_taylor(x,N));
+
+    return 0;
+}
